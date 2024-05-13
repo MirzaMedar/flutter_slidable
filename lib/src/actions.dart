b4d40680ce3d5c716a9789bf56d5bc43e433cb74
Mirza Medar <mirza_medar@outlook.com>
2024-05-13 21:54:38 +0200

    Flutter upgrade

--- lib/src/actions.dart
+++ lib/src/actions.dart
@@ -99,8 +99,6 @@ class CustomSlidableAction extends StatelessWidget {
           style: OutlinedButton.styleFrom(
             padding: padding,
             backgroundColor: backgroundColor,
-            primary: effectiveForegroundColor,
-            onSurface: effectiveForegroundColor,
             shape: RoundedRectangleBorder(
               borderRadius: borderRadius,
             ),
